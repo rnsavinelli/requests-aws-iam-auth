08105acc2d7d4b4e75f42c42daf089f7b165c37f
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-19 14:11:07 -0300

    remove author's deployment environment

--- example/usage_example.py
+++ example/usage_example.py
@@ -2,7 +2,8 @@ def test_example(hostpath):
     import requests
     import requests_aws_iam_auth
 
-    # Provide AWS creds and config per Boto3 specs: https://tknk.io/ri3v
+
+
     auth = requests_aws_iam_auth.ApiGateway()
 
     requests.get("https://" + hostpath, auth=auth)
--- setme.dev-env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export DOCKER_HUB_ACCESS_TOKEN=
-export TWINE_PASSWORD=
-export TWINE_PASSWORD_TEST=
