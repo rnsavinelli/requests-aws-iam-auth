da1480bafa14bf8c9a02d7257e010c2fb78566da
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2021-07-19 14:28:45 -0300

    update example

--- example/usage_example.py
+++ example/usage_example.py
@@ -1,9 +1,15 @@
-def test_example(hostpath):
+def usage_example(hostpath):
     import requests
     import requests_aws_iam_auth
 
+    aws_access_key_id='AKIAIOSFODNN7EXAMPLE'
+    aws_secret_access_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+    region_name='aws-example-region-1'
 
-
-    auth = requests_aws_iam_auth.ApiGateway()
+    auth = requests_aws_iam_auth.ApiGateway(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
 
     requests.get("https://" + hostpath, auth=auth)
